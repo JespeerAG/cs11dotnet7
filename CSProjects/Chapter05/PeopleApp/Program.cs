ecff7f26bfc76c385edbd488b95f2fe4a57a5e92
Tobia Beccari <tobia.beccari@gmail.com>
2023-01-16 22:29:56 +0000

    Begin Chapter05

---
+++ CSProjects/Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Packt.Shared;
+
+Person bob = new();
+string format = "{0} was born on {1:dddd, d MMMM yyyy}";
+
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new DateTime(1965, 12, 22);
+
+WriteLine(format, bob.Name, bob.DateOfBirth);
+
+Person alice = new()
+{
+    Name = "Alice Jones",
+    DateOfBirth = new(1998, 3, 7)
+};
+
+WriteLine(format, bob.Name, bob.DateOfBirth);
