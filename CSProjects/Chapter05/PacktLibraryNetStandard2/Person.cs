9fcf7e9c5d79cd58ca919930e5b95d9031345773
Tobia Beccari <tobia.beccari@gmail.com>
2023-01-16 23:16:40 +0000

    Add things to Person class

--- CSProjects/Chapter05/PacktLibraryNetStandard2/Person.cs
+++ CSProjects/Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using PacktLibraryNetStandard2;
+
 namespace Packt.Shared
 {
     public class Person : System.Object
     {
         public string? Name;
         public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+
+        public List<Person> Children = new();
     }
 }
